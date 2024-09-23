c785d136aaa5b9099f523ccd388dd4ebac96b1ad
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-09-23 11:33:44 +0200

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Lorenzo Menino on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
